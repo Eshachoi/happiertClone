68bf49d297dce9c5032bca49862c6cdb4b6b9574
Eshachoi <eshachoi@naver.com>
2025-09-23 14:18:28 +0900

    수정
    수정

--- script/script.js
+++ script/script.js
@@ -27,13 +27,14 @@ function headerShow(){
 //스크롤 페이드인인터랙션
 const content = document.querySelectorAll(".content");
 window.addEventListener("scroll", ()=>{
-    const winH = window,innerHeight;
+    const winH = window.innerHeight;
     for(let i = 0; i < content.length; i++){
         const contentTop = content[i].getBoundingClientRect().top;
+        
         if(contentTop - winH < 0){
-            content[i].classList.add("in");
+            content[i].classList.add("in");//화면 안에 들어오면 in 클래스 추가
         }else{
-            
+            content[i].classList.remove("in");//화면 밖에 나가면 in 클래스 제거
         }
     }
-    }
+});
